174c2ec74f90718ecc90a4806b93c976bee30c65
Crowdbotics <team@crowdbotics.com>
2024-02-21 07:51:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-band-46522",
+  url: "https://bitter-band-46522.botics.co",
+  api: "https://bitter-band-46522.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
